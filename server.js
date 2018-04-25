310d2cad33540f09d11be9b2b70e29f7c59bd52d
Jarold Wong <jaroldw@gmail.com>
2018-04-25 16:30:02 -0700

    Additional setup for serving up client

--- server.js
+++ server.js
@@ -30,5 +30,16 @@ app.use((error, req, res, next) => {
   });
 });
 
+if (process.env.NODE_ENV === 'production') {
+  // serve up production assets like main.js, main.css
+  app.use(express.static('client/build'));
+
+  // server up index.html file if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Running on port ${PORT}`));
